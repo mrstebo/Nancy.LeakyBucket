617f580f7c564f7af14af2ea73bcd6a8ba4ab440
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-07-13 10:45:30 +0100

    Updated the DefaultClientIdentifier so that it can be found in a dictionary.

--- src/Nancy.LeakyBucket/Identifiers/DefaultClientIdentifier.cs
+++ src/Nancy.LeakyBucket/Identifiers/DefaultClientIdentifier.cs
@@ -2,6 +2,22 @@
 {
     public class DefaultClientIdentifier : IClientIdentifier
     {
-        public string UserAgentAddress { get; set; }
+        public string UserAgentAddressAddress { get; }
+
+        public DefaultClientIdentifier(string userAgentAddress)
+        {
+            UserAgentAddressAddress = userAgentAddress;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is DefaultClientIdentifier &&
+                   ((DefaultClientIdentifier) obj).UserAgentAddressAddress == UserAgentAddressAddress;
+        }
+
+        public override int GetHashCode()
+        {
+            return UserAgentAddressAddress?.GetHashCode() ?? 0;
+        }
     }
 }
